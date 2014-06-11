9bfe0b0b6fb56fd18846e99bab140b0abbb0617b
Ekramvis <ekramvis@gmail.com>
2014-06-11 11:53:08 -0400

    Update config.js
    allow inline styles

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -8,7 +8,10 @@ CKEDITOR.editorConfig = function( config )
   // Define changes to default configuration here. For example:
   // config.language = 'fr';
   // config.uiColor = '#AADC6E';
-
+  
+  /* Allow inline style */
+  config.allowedContent = true;
+  
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
   config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
